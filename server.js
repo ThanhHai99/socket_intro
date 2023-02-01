6d8d45bee88c961e4a7f065ca5faeeb9eda25d2d
HaiTran <hai.tran@kyanon.digital>
2023-02-02 00:07:53 +0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const socketio = require('socket.io')
-const http = require('http')
+const net = require('net')
+const {WebSocket} = require('ws')
 
-const app = express()
-let server = http.createServer(app)
-let io = socketio(server)
+// Config port
+const ssPort = 3000
+const wssPort = 3001
 
-const PORT = 3000
-server.listen(PORT, () => console.log(`App running at : ${PORT}`))
+// Init sockets & rooms
+let sockets = []
+let rooms = []
+
+// RawSocket
+const socketServer = net.createServer()
+socketServer.listen(ssPort, () => {
+    console.log(`Socket server listening at ${ssPort}`)
+})
+
+
+// WebSocket
+const webSocketServer = new WebSocket.Server({port: wssPort})
+webSocketServer.once("listening", () => {
+    console.log(`Web socket server listening at ${wssPort}`)
+})
+webSocketServer.on('connection', (ws) => {
+    sockets.push(ws)
+})
+
+setInterval(() => {
+    sendBroadcast()
+}, 1000)
+
+const sendBroadcast = () => {
+    sockets.forEach(socket => {
+        const currentTime = new Date()
+        console.log(`Send broadcast ${currentTime}`)
+        // socket.wri(currentTime)
+    })
+}
